a3786e15e188ad124e9e4254132c259dfce347b6
DheerendraKumar01 <89966259+DheerendraKumar01@users.noreply.github.com>
2021-09-11 08:58:39 +0530

    Add files via upload

---
+++ Assignment_ExceptionHandling_09_Sep_2021/ExceptioninHandlingInNumber.java
@@ -0,0 +1,22 @@
+package com.Assignment_ExceptionHandling_09_Sep_2021;
+
+import java.util.Scanner;
+
+
+public class ExceptioninHandlingInNumber {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter you Integer :");
+		String num = scanner.nextLine();
+
+		try {
+			int num1 = Integer.parseInt(num);
+			System.out.println("The square value is : "+ num1 * num1);
+		}
+		catch(NumberFormatException e){
+			System.out.println("Entered input is not a valid format for an integer");
+		}
+	}
+
+
+}
